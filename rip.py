92955761c82caaad5f0980d52b92aefdc0dea94f
jla201 <jla201@uclive.ac.nz>
2023-04-09 21:16:25 +1200

    Refactoring

--- rip.py
+++ rip.py
@@ -1,25 +1,17 @@
 import socket
-import sys, getopt
+import sys
+
 import select
 import argparse
 import time
 import json
 
-"""
-To Do
-Timers for router entries
-    - Add timers to routing_table -- tick
-    - make timers count up
-
-Send a bytearray out of the routing table
-    - create conversion function
-    - create function to covert this back
-"""
-
 
 
 """
-Current timer has flaw where reading will muck up how often it is polled
+Notes on potential issues
+Current timer might have flaw where reading will muck up how often it is polled
+this probably dosn't matter
 """
 
 
@@ -32,51 +24,41 @@ python rip.py config
 I made it add the .txt might change this later
 """
 
+
 class RipDaemon:
     def __init__(self, config_name):
         """ Class to create and manage a RIP daemon """
         print("Daemon created")
         daemon_alive = True
 
-        """Unique identifire for this router"""
-        self.router_id = 0
+        """ Parse config file for valid id, ports and outputs"""
+        daemon_input = RipDaemon.read_config(config_name)
 
+        if daemon_input == -1:
+            self.end_daemon()
 
-        """[port, distance, id]"""
-        self.outputs = []
+        self.router_id, self.input_ports, self.outputs = daemon_input
 
-        """ Ports to listen on"""
-        self.input_ports = []
+        # Setup sockets to receive data with
+        self.input_sockets = self.socket_setup(self.input_ports)
 
-        """Stores sockets"""
-        self.input_sockets = []
+        # Generic socket used to send updates
+        self.output_socket = self.input_sockets[0]
 
-        """ router_id : [port, metric, timer]"""
+        # router_id : [port, metric, timer]
         self.routing_table = {}
-
-        """ Used to tell when to send advts"""
-        self.advt_counter = 0
-
-        self.read_config(config_name)
-
-        output = self.convert_config()
-
-        print(output)
-
-        self.validate_config()
-
         self.create_table()
 
-        self.socket_setup()
-
-        # Ports to wait on input
+        # Ports ready to read from
         self.readable = []
-        # Output ports
+        # Used to tell if the port can be written to
         self.writeable = []
         # Need to look into this
         self.exceptional = []
 
-        # Time in between sending update things (in seconds)
+        # Used to tell when to send advts
+        self.advt_counter = 0
+        # Interval to check output status
         self.timeout = 10
         # Time timer started
         self.start = 0
@@ -89,14 +71,14 @@ class RipDaemon:
             # Main loop
             readable, writeable, exceptional = select.select(self.input_sockets, [], [], self.timeout)
 
-            # Might need to make this multithreaded
+            # Might need to make this use multithreading
 
             if len(readable) != 0:
                 print("Read from sockets")
                 self.read_input(readable)
 
             if len(writeable) != 0:
-                # Probably wont want this one
+                # Probably won't want this one
                 print("Write to sockets")
 
             if len(exceptional) != 0:
@@ -138,11 +120,10 @@ class RipDaemon:
 
         print("***********************\n")
 
-
     def check_timer(self):
         """ Checks if its time to send router adverts"""
 
-        if(self.start < time.time()):
+        if self.start < time.time():
             self.start = time.time() + self.timeout
             return True
         return False
@@ -151,6 +132,7 @@ class RipDaemon:
         """ Update the time since an entry was last updated"""
         print("do this later")
         print("not important")
+        self.display_details()
 
     def create_table(self):
         for output in self.outputs:
@@ -168,14 +150,14 @@ class RipDaemon:
                 self.routing_table[id] = [self.routing_table[peer_id][0], (new_data[id][1] + self.routing_table[peer_id][1]), 0]
             else:
                 new = new_data[id][1] + self.routing_table[peer_id][1]
-                if new  < self.routing_table[id][1]:
+                if new < self.routing_table[id][1]:
                     self.routing_table[id] = [self.routing_table[peer_id][0], new, 0]
 
     def encode_table(self):
         print("table encoded")
 
         data = json.dumps({"data": [self.routing_table, self.router_id]})
-        return(bytes(data, encoding="utf-8"))
+        return bytes(data, encoding="utf-8")
 
     def int_keys(self, received_table):
         new_table = {}
@@ -195,8 +177,7 @@ class RipDaemon:
         return received_table, next_hop
 
     def send_updates(self):
-        # Do later
-
+        """ Sends the routers table to its neighbour routers"""
         print(self.routing_table)
 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -214,16 +195,20 @@ class RipDaemon:
         return data
 
 
-
-
-    def socket_setup(self):
+    @staticmethod
+    def socket_setup(input_ports):
         """ Creates udp sockets """
-        for port in self.input_ports:
+        input_sockets = []
+
+        for port in input_ports:
             udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             udp_socket.bind(("127.0.0.1", port))
-            self.input_sockets.append(udp_socket)
+            input_sockets.append(udp_socket)
 
-    def read_config(self, config_name):
+        return input_sockets
+
+    @staticmethod
+    def read_config(config_name):
         """ Reads the configuration file """
 
         config_file = open(config_name)
@@ -236,94 +221,89 @@ class RipDaemon:
             # Cleans up any newlines and whitespace
             config_dict[variable] = [value.strip() for value in value.split(",")]
 
-        self.router_id = config_dict["router_id"][0]
-        self.input_ports = config_dict["input_ports"]
-        self.outputs = config_dict["outputs"]
+        router_id = config_dict["router_id"][0]
+        input_ports = config_dict["input_ports"]
+        outputs = config_dict["outputs"]
 
-    def convert_config(self):
-        correct_input = []
+        formatted_config = RipDaemon.convert_config(router_id, input_ports, outputs)
+
+        if formatted_config[0] != 1:
+            print(formatted_config[1])
+            return -1
+
+        router_id, input_ports, outputs = formatted_config[1]
+
+        RipDaemon.validate_config(router_id, input_ports, outputs)
+
+        return router_id, input_ports, outputs
+
+    @staticmethod
+    def convert_config(router_id, input_ports, outputs):
+        """Transforms the raw data from the configuration file into usable data"""
+
+        if router_id.isdigit():
+            router_id = int(router_id)
+        else:
+            return -1, "Router id is an invalid value"
+
+        input_ports = [int(port) if port.isdigit() else None for port in input_ports]
+        if None in input_ports:
+            return -1, "An input port is an invalid value"
+
         correct_output = []
-        """Transforms the raw data from the configuration file into data readable by the validate_config function"""
-
-        """Error codes:
-            1: Router id is not a correct id
-            2: An input port doesnt have a correct port number
-            3: An output port has incorrect syntax
-            4: An output port number is incorrect
-            5: An outputs metric value is incorrect
-            6: An outputs peer router id is incorrect"""
-        #todo for clean up:
-        #change hold variables to better names
-        #maybe do something with error codes i.e. return an error message/make a function to do that
-        try:
-            self.router_id = int(self.router_id)
-
-        except ValueError:
-            return 1
-
-        for i_port_string in self.input_ports:
-            i_port = self.validate_int(i_port_string)
-            correct_input.append(i_port)
-        self.input_ports = correct_input
-
-        for output_string in self.outputs:
+        for output_string in outputs:
             output = output_string.split("-")
-            print(output)
+
             if len(output) == len(output_string):
                 return 3
 
-            o_port = self.validate_int(output[0])
-            o_r_id = self.validate_int(output[2])
-            o_metric = self.validate_int(output[1])
-            correct_output.append((o_port, o_metric, o_r_id))
-        self.outputs = correct_output
-        return 0
+            if output[0].isdigit() and output[1].isdigit() and output[2].isdigit():
+                output_port = int(output[0])
+                output_metric = int(output[1])
+                output_id = int(output[2])
+            else:
+                return -1, "An output value is invalid"
 
-    def validate_int(self, value):
-        if value.isdigit():
-            return int(value)
-        #talk to james about making a function that stops process because of error
-        #return False
+            correct_output.append((output_port, output_metric, output_id))
 
-    def validate_config(self):
+        return 1, (router_id, input_ports, correct_output)
+
+    @staticmethod
+    def validate_config(router_id, input_ports, outputs):
         """ Checks  all values in config for correctness"""
-
-        """
-        Error codes:
-            1: Router id not in integer range
-            2: 2 or more input ports have the same port number
-            3: An input port number has not in range
-            4: Output port number is the same as Input port number
-            5: An output port number has not in range
-            6:
-        """
 
         max_port = 64000
         max_id = 64000
 
-        if self.router_id > 1 and self.router_id < max_id:
-            return 1
-        if len(set(self.input_ports)) != len(self.input_ports):
-            return 2
-        for i_port in self.input_ports:
+        if 1 < router_id < max_id:
+            return -1, "Router id not in integer range"
+        if len(set(input_ports)) != len(input_ports):
+            return -1, "2 or more input ports have the same port number"
+        for i_port in input_ports:
             if i_port < 1024 or i_port > max_port:
-                return 3
-        for output in self.outputs:
-            if output[0] in self.input_ports:
-                return 4
+                return -1, "An input port number has not in range"
+        for output in outputs:
+            if output[0] in input_ports:
+                return -1, "Output port number is the same as Input port number"
             if output[0] < 1024 or i_port > max_port:
-                return 5
-            #check if output[1,2] are ints
-        return 0
+                return -1, "An output port number has not in range"
+            # check if output[1,2] are ints
+        return 1, "All good"
+
+    def end_daemon(self):
+        print("Destroying daemon")
+        self.display_details()
+        sys.exit()
+
 
 if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
-    argParser.add_argument('filename',help="config filename")
+    argParser.add_argument('filename', help="config filename")
 
     args = argParser.parse_args()
-    config_name = args.filename + ".txt"
+    config_imput = args.filename + ".txt"
 
-    if config_name is None:
+    if config_imput is None:
         print("No config file name was given")
     else:
-        rip_daemon = RipDaemon(config_name)
+        rip_daemon = RipDaemon(config_imput)
